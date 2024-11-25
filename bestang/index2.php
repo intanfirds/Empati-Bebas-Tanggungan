964ce7e2033910ba25c4bd76781ce2b4f5d5174c
siskanuriaprilia <aprilias878@gmail.com>
2024-11-25 13:51:15 +0700

    Bestang - TI

--- bestang/index2.php
+++ bestang/index2.php
@@ -66,13 +66,6 @@ session_start();
         <!-- Divider -->
         <hr class="sidebar-divider my-0" />
 
-        <!-- Nav Item - Profile -->
-        <li class="nav-item">
-          <a class="nav-link collapsed" href="#">
-            <i class="fas fa-user"></i>
-            <span>Profile</span>
-          </a>
-        </li>
 
         <!-- Divider -->
         <hr class="sidebar-divider my-0" />
@@ -138,71 +131,7 @@ session_start();
 
             <!-- Topbar Navbar -->
             <ul class="navbar-nav ml-auto">
-              <!-- Nav Item - Alerts -->
-              <li class="nav-item dropdown no-arrow mx-1">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="alertsDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i class="fas fa-bell fa-fw"></i>
-                  <!-- Counter - Alerts -->
-                  <span class="badge badge-danger badge-counter">3+</span>
-                </a>
-                <!-- Dropdown - Alerts -->
-                <div
-                  class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                  aria-labelledby="alertsDropdown"
-                >
-                  <h6 class="dropdown-header">Alerts Center</h6>
-                  <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="mr-3">
-                      <div class="icon-circle bg-primary">
-                        <i class="fas fa-file-alt text-white"></i>
-                      </div>
-                    </div>
-                    <div>
-                      <div class="small text-gray-500">December 12, 2019</div>
-                      <span class="font-weight-bold"
-                        >A new monthly report is ready to download!</span
-                      >
-                    </div>
-                  </a>
-                  <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="mr-3">
-                      <div class="icon-circle bg-success">
-                        <i class="fas fa-donate text-white"></i>
-                      </div>
-                    </div>
-                    <div>
-                      <div class="small text-gray-500">December 7, 2019</div>
-                      $290.29 has been deposited into your account!
-                    </div>
-                  </a>
-                  <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="mr-3">
-                      <div class="icon-circle bg-warning">
-                        <i class="fas fa-exclamation-triangle text-white"></i>
-                      </div>
-                    </div>
-                    <div>
-                      <div class="small text-gray-500">December 2, 2019</div>
-                      Spending Alert: We've noticed unusually high spending for
-                      your account.
-                    </div>
-                  </a>
-                  <a
-                    class="dropdown-item text-center small text-gray-500"
-                    href="#"
-                    >Show All Alerts</a
-                  >
-                </div>
-              </li>
-
+             
               <div class="topbar-divider d-none d-sm-block"></div>
 
               <!-- Nav Item - User Information -->
@@ -276,7 +205,7 @@ session_start();
                     <div class="card-body">
                       <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                          <div class="h5 mb-0 font-weight-bold text-primary">5</div>
+                          <div class="h5 mb-0 font-weight-bold text-primary">10</div>
                           <div class="text-xs font-weight-bold mb-1">
                             Jumlah Mahasiswa yang sudah mengupload File Laporan PKL
                           </div>
@@ -295,7 +224,7 @@ session_start();
                       <div class="card-body">
                         <div class="row no-gutters align-items-center">
                           <div class="col mr-2">
-                            <div class="h5 text-info mb-0 font-weight-bold">4</div>
+                            <div class="h5 text-info mb-0 font-weight-bold">5</div>
                             <div class="text-xs font-weight-bold mb-1">
                               Jumlah Mahasiswa yang sudah mengupload File Laporan Skripsi
                             </div>
@@ -314,7 +243,7 @@ session_start();
                       <div class="card-body">
                         <div class="row no-gutters align-items-center">
                           <div class="col mr-2">
-                            <div class="text-warning h5 text-info mb-0 font-weight-bold">7</div>
+                            <div class="text-warning h5 text-info mb-0 font-weight-bold">20</div>
                             <div class="text-xs font-weight-bold mb-1">
                               Jumlah Mahasiswa yang sudah mengupload File Bebas Kompen
                             </div>
@@ -333,7 +262,7 @@ session_start();
                       <div class="card-body">
                         <div class="row no-gutters align-items-center">
                           <div class="col mr-2">
-                            <div class="h5 text-success mb-0 font-weight-bold">10</div>
+                            <div class="h5 text-success mb-0 font-weight-bold">11</div>
                             <div class="text-xs font-weight-bold mb-1">
                               Jumlah Mahasiswa yang sudah mengupload Scan TOEIC (min skor 450)
                             </div>
@@ -347,7 +276,7 @@ session_start();
                   </div>
                 </div>
                   <!-- Footer -->
-                  <footer class="sticky-footer bg-white fixed-bottom">
+                  <footer class="sticky-footer bg-white fixed-bottom" style= "padding: 5px 0 ;">
                     <div class="container my-auto">
                       <div class="copyright text-center my-auto">
                         <span
