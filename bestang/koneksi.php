226797780197495ff1f706695ade57b764df6156
siskanuriaprilia <aprilias878@gmail.com>
2024-12-04 10:08:55 +0700

    update

--- bestang/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$connInfo = array("Database"=>"bebastanggungan", "UID"=>"", "PWD"=>"");
-$conn = sqlsrv_connect($host, $connInfo);
-
-if( !$conn ) {
-    die( print_r( sqlsrv_errors(), true));
-}
-?>
