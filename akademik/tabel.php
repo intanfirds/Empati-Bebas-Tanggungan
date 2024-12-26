8ad4d8d7ee72404d4681dcd39284f5e607047467
JacktheRipper2187 <ulilf442@gmail.com>
2024-12-26 18:58:40 +0700

    memperbaiki href logout di bagian tabel

--- akademik/tabel.php
+++ akademik/tabel.php
@@ -372,7 +372,7 @@ $_SESSION['nip_admin'] = $data_admin['nip'];
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="index-admin.html">Logout</a>
+                    <a class="btn btn-primary" href="\Empati-Bebas-Tanggungan\index-admin.html">Logout</a>
                 </div>
             </div>
         </div>
