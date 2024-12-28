afee1ef858cfe4478a5af82f54282622c3256492
JacktheRipper2187 <ulilf442@gmail.com>
2024-12-28 22:15:37 +0700

    memperbaiki OOP pada login.php

--- login.php
+++ login.php
@@ -2,7 +2,87 @@
 session_start();
 require_once 'koneksi.php'; // Menghubungkan ke database
 
-class AdminLogin
+// Interface untuk autentikasi
+interface AuthInterface
+{
+    public function authenticate($username, $password);
+}
+
+// Abstract class untuk pengguna
+abstract class User
+{
+    protected $name;
+    protected $nip;
+    protected $role;
+
+    public function __construct($name, $nip, $role)
+    {
+        $this->name = $name;
+        $this->nip = $nip;
+        $this->role = $role;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    abstract public function redirect();
+}
+
+// Class turunan untuk setiap role
+class Jurusan extends User
+{
+    public function redirect()
+    {
+        header("Location: /Empati-Bebas-Tanggungan/jurusan/index.php");
+        exit;
+    }
+}
+
+class AdminTI extends User
+{
+    public function redirect()
+    {
+        header("Location: /Empati-Bebas-Tanggungan/bestang/bestang ti/index.php");
+        exit;
+    }
+}
+
+class AdminSIB extends User
+{
+    public function redirect()
+    {
+        header("Location: /Empati-Bebas-Tanggungan/bestang/bestang sib/index.php");
+        exit;
+    }
+}
+
+class Akademik extends User
+{
+    public function redirect()
+    {
+        header("Location: /Empati-Bebas-Tanggungan/akademik/index.php");
+        exit;
+    }
+}
+
+class Perpustakaan extends User
+{
+    public function redirect()
+    {
+        header("Location: /Empati-Bebas-Tanggungan/perpustakaan/index.php");
+        exit;
+    }
+}
+
+// Kelas utama untuk login
+class AdminLogin implements AuthInterface
 {
     private $conn;
 
@@ -14,14 +94,15 @@ class AdminLogin
     public function authenticate($username, $password)
     {
         $query = "SELECT nama, nip, role, password FROM admin WHERE username = ?";
-        $params = array($username);
+        $params = [$username];
         $stmt = sqlsrv_prepare($this->conn, $query, $params);
 
         if (sqlsrv_execute($stmt)) {
             if ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
                 if (password_verify($password, $row['password'])) {
-                    $this->setSession($row);
-                    $this->redirectUser($row['role']);
+                    $user = $this->createUserObject($row);
+                    $this->setSession($user);
+                    $user->redirect();
                 } else {
                     $this->showAlertAndRedirect('Password salah!', 'index-admin.html');
                 }
@@ -33,36 +114,28 @@ class AdminLogin
         }
     }
 
-    private function setSession($userData)
+    private function createUserObject($row)
     {
-        $_SESSION['nama_admin'] = $userData['nama'];
-        $_SESSION['nip_admin'] = $userData['nip'];
-        $_SESSION['role'] = $userData['role'];
-    }
-
-    private function redirectUser($role)
-    {
-        switch ($role) {
+        switch ($row['role']) {
             case 'Jurusan':
-                header("Location: /Empati-Bebas-Tanggungan/jurusan/index.php");
-                break;
+                return new Jurusan($row['nama'], $row['nip'], $row['role']);
             case 'Admin TI':
-                header("Location: /Empati-Bebas-Tanggungan/bestang/bestang ti/index.php");
-                break;
+                return new AdminTI($row['nama'], $row['nip'], $row['role']);
             case 'Admin SIB':
-                header("Location: /Empati-Bebas-Tanggungan/bestang/bestang sib/index.php");
-                break;
+                return new AdminSIB($row['nama'], $row['nip'], $row['role']);
             case 'Akademik':
-                header("Location: /Empati-Bebas-Tanggungan/akademik/index.php");
-                break;
+                return new Akademik($row['nama'], $row['nip'], $row['role']);
             case 'Perpustakaan':
-                header("Location: /Empati-Bebas-Tanggungan/perpustakaan/index.php");
-                break;
+                return new Perpustakaan($row['nama'], $row['nip'], $row['role']);
             default:
                 $this->showAlertAndRedirect('Role tidak dikenali!', 'index-admin.html');
-                break;
         }
-        exit;
+    }
+
+    private function setSession(User $user)
+    {
+        $_SESSION['nama_admin'] = $user->getName();
+        $_SESSION['role'] = $user->getRole();
     }
 
     private function showAlertAndRedirect($message, $url)
@@ -72,6 +145,7 @@ class AdminLogin
     }
 }
 
+// Proses login
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
